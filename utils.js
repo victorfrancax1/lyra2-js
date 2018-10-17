67dde4a520dfdd111c62e7c9672739fe67817227
Victor França <victhor_1996@hotmail.com>
2018-10-17 11:06:35 -0300

    adding new helper functions

--- utils.js
+++ utils.js
@@ -4,6 +4,19 @@ function pad(num, size) {
     return s;
 }
 
-module.exports{
-	pad: pad
+//receives long array (blake2b's state) and returns a binary string array with 64bit padding
+function long64Pad(arr){
+	var padded = arr.map((x) => pad(x.toString(2), 64));
+	return padded;
+}
+
+//receives long array (blake2b's state) and returns 1024bit binary string
+function longStringify(arr){
+	var result = long64Pad(arr).reduce((x,y) => x + y);
+	return result;
+}
+
+module.exports = {
+	long64Pad: long64Pad,
+	longStringify: longStringify
 }
